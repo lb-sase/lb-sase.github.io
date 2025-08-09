9c6bd8b55dd43c0358c278b2d64e45846d459521
Jules-Elvin Andrade <juleselvandrade@gmail.com>
2025-08-09 07:31:08 -0700

    first draft: main outline and format added, need to add actual details + responsiveness in future drafts

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// code executes on window load 
+window.onload = function() {
+    document.getElementById('landing-page-inner').classList.add('revealed');
+
+    console.log("the entire page has loaded!");
+
+};
