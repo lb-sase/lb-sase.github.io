608af9fb79d7c1d98d0fd446ab7743851ef2df03
Jules-Elvin Andrade <juleselvandrade@gmail.com>
2025-08-29 22:16:34 -0700

    draft: big revamp

--- script.js
+++ script.js
@@ -1,8 +1,56 @@
 
 // code executes on window load 
 window.onload = function() {
-    document.getElementById('landing-page-inner').classList.add('revealed');
 
+    // first command that'll be run :)
     console.log("the entire page has loaded!");
 
+    // added functionality to detect which page the user is on so that can be a global js file 
+    const currentPath = window.location.pathname;
+    let actual_path = currentPath.substring(currentPath.lastIndexOf('/'), currentPath.length);
+
+    if (actual_path == "/index.html") {
+        document.getElementById('landing-page-inner').classList.add('revealed');
+
+        document.onscroll = function() {
+            if (window.innerHeight + window.scrollY >= document.body.clientHeight) {
+                const element = document.getElementById("scroll-down-container");
+                element.classList.add("fade-out");
+            } else {
+                const element = document.getElementById("scroll-down-container");
+                element.classList.remove("fade-out");
+            }
+        }
+    }
+
+    // navbar reveal/disappear on menu button press 
+    const menu = document.getElementById("bi-list");
+    const navbar = document.getElementById("sase-navbar-cntr");
+    let clicker = 0;
+    
+    menu.addEventListener('click', () => {
+        clicker++;
+        if (clicker % 2 == 1) { // shown
+            navbar.style.display = 'flex';
+            navbar.style.animation = 'slide-in 1s';
+        } else { // hidden
+            navbar.style.display = 'none';
+            navbar.style.animation = 'none';
+        }
+    })
+
+    // observing #sase-info
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                // header is in the viewport, animate svg
+                const path = document.getElementById("ul-stroke-path");
+                path.style.animation = "draw 2.5s ease-in-out forwards";
+                console.log('AHHHHHHHHH');
+            }
+        });
+    });
+
+    const targetElement = document.getElementById('sase-info-header');
+    observer.observe(targetElement);
 };
